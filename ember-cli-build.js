e46ed296ea45527dcc574c744f96edc7bb38a1e8
Chris Garrett <cgarrett@linkedin.com>
2020-05-10 22:05:08 -0700

    add polyfill for modern array methods

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,10 @@ module.exports = function (defaults) {
     autoImport: {
       exclude: ['@glimmer/tracking'],
     },
+
+    'ember-cli-babel': {
+      includePolyfill: true,
+    },
   });
 
   /*
